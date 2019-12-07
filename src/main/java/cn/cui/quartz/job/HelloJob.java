1ad875283357d4e99447054a0de71995b234a181
cuihongliang <1442802633@qq.com>
2019-12-07 09:33:51 +0800

    王美佳提交

--- src/main/java/cn/cui/quartz/job/HelloJob.java
+++ src/main/java/cn/cui/quartz/job/HelloJob.java
@@ -7,6 +7,6 @@ public class HelloJob implements org.quartz.Job {
 	 @Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		// TODO Auto-generated method stub
-		
+		System.out.println("我是王美佳");
 	}
 }
